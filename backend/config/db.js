71c89d0face3acf45335db34ccaa56f1c2f4b3a1
Pothapragada Gnana Chaithanya <pchaithanya21@gmail.com>
2026-03-03 16:34:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,13 @@
-module.exports = {};
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected ✅");
+  } catch (error) {
+    console.error("MongoDB connection failed ❌", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
