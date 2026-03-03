a6deae57bde5215e60cd23cf8d2385464b320926
Pothapragada Gnana Chaithanya <pchaithanya21@gmail.com>
2026-03-03 16:34:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
 
 const app = express();
+
+connectDB();   // THIS MUST BE HERE
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +16,8 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
