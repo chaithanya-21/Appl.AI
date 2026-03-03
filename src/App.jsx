ef31fcd854c1275366fc8874823f6785bc53b9b3
Pothapragada Gnana Chaithanya <pchaithanya21@gmail.com>
2026-03-03 17:07:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        {/* Public Pages */}
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
