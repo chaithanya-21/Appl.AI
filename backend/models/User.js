0c8909fa5ea8f2ba6556ef9c3464516938a48a89
Pothapragada Gnana Chaithanya <pchaithanya21@gmail.com>
2026-03-03 15:36:06 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,22 @@
-module.exports = {};
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  backupEmail: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  resetToken: String,
+  resetTokenExpiry: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
